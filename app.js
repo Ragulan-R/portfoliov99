e736a92fba408f7bda4cd9cd23046cc3ee351d68
ragulan <ragulanravi21@gmail.com>
2021-09-29 11:31:50 -0400

    code cleanup

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-const menu = document.querySelector('#mobile-menu');
-const menuLinks = document.querySelector('.navbar__menu');
+const menu = document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('.navbar__menu')
+const navLinks = document.querySelectorAll('.navbar__links')
 
-menu.addEventListener('click', function() {
-  menu.classList.toggle('is-active');
-  menuLinks.classList.toggle('active');
-});
+menu.addEventListener('click', function () {
+  menu.classList.toggle('is-active')
+  menuLinks.classList.toggle('active')
+})
+
+// only works on home button
+navLinks.addEventListener('click', function () {
+  menu.classList.toggle('is-active')
+  menuLinks.classList.toggle('active')
+})
